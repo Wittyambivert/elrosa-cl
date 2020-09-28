a774f5f7d9cf4f7271a9717a6edb4a9b14daf74e
Max <wittyambivert.com>
2020-09-27 23:54:12 -0800

    settle header

---
+++ app/assets/dev/gulp/styles.js
@@ -0,0 +1,13 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const importCSS = require("postcss-import");
+const autoprefixer = require("autoprefixer");
+
+gulp.task("styles", ["icons"], () => {
+    return gulp.src("./app/assets/dev/styles/main-style.scss")
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(postcss([importCSS, autoprefixer]))
+        .pipe(gulp.dest("./app/assets/prod/styles"));
+});
---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+require("./app/assets/dev/gulp/icons");
+require("./app/assets/dev/gulp/styles");
+require("./app/assets/dev/gulp/watch");
+
+
